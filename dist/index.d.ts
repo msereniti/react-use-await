d16f1c6ef8471f071199909504c98a5a9dc5c8c8
Mikhail Sereniti @phytonmk <phytonmk@gmail.com>
2022-01-20 01:08:09 +0300

    chore: deleted build artifacts from repo

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare const usePromise: <Result extends unknown, Args extends any[]>(promise: (...inputs: Args) => Promise<Result>, inputs: Args, lifespan?: number) => Result;
-export default usePromise;
