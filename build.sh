dacb1d3a60de7eaea5ba87d6a64e37439d9cd525
Mikhail Sereniti @phytonmk <phytonmk@gmail.com>
2022-05-24 17:16:05 +0300

    build only with tsc

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
 rm -rf dist &&
-tsc --outDir dist &&
-
-esbuild --minify --sourcemap src/useAwait.tsx --outfile=dist/index.js --target=esnext --format=cjs && 
-esbuild --minify --sourcemap src/useAwait.tsx --outfile=dist/index.mjs --target=esnext --format=esm 
+tsc --outDir dist 
